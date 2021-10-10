b6252746e49a04f5e3f9900bd372bb2c246c5ae4
Claus Bjerre Jacobsen <claus@aneks.dk>
2021-10-10 21:23:23 +0200

    #Update 2
    Player can now jump
    
    Cinemamachine + polybrush added to project

--- Assets/MyProject/Scripts/PlayerControl.cs
+++ Assets/MyProject/Scripts/PlayerControl.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 5f;
+    public LayerMask groundLayer;
+    public float groundCheckLength = 0.55f;
+    public float jumpForce = 100f;
 
     float horizontalMovement;
     float verticalMovement;
     Rigidbody rigidbody;
     Vector3 movement;
+    bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +28,14 @@ public class PlayerControl : MonoBehaviour
         horizontalMovement = Input.GetAxis("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
         movement = new Vector3(horizontalMovement, 0, verticalMovement).normalized;
+
+        // kode der tjekker om vi er tæt på jorden
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckLength, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce);
+        }
     }
 
     private void FixedUpdate()
